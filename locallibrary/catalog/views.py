9965a688d31bc1270d1330a8ab825e1dc2474feb
fis777 <fqibymeksm@protonmail.com>
2018-10-12 15:11:24 +0500

    add landing page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.db.models import Manager
+from .models import Book, BookInstance, Author, Genre
+
+class objects(Manager):
+    pass
 
 # Create your views here.
+def index(request):
+    ''' Отображение главной страницы '''
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a')
+    num_authors = Author.objects.count()
+
+    return render(request, 'index.html', 
+                    context={'num_books': num_books,
+                            'num_instances': num_instances,
+                            'num_instances_available': num_instances_available,
+                            'num_authors': num_authors})
+
