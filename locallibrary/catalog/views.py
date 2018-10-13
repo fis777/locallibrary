f5bd5979acd7b9149e862533753bd26aad1ed3b4
fis777 <fis777@gmail.com>
2018-10-13 11:16:46 +0500

    views module  format to PEP8

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,11 +1,9 @@
+''' Django views is here'''
 from django.shortcuts import render
-from django.db.models import Manager
-from .models import Book, BookInstance, Author, Genre
-
-class objects(Manager):
-    pass
+from .models import Book, BookInstance, Author
 
 # Create your views here.
+
 def index(request):
     ''' Отображение главной страницы '''
     num_books = Book.objects.count()
@@ -13,9 +11,8 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a')
     num_authors = Author.objects.count()
 
-    return render(request, 'index.html', 
-                    context={'num_books': num_books,
-                            'num_instances': num_instances,
-                            'num_instances_available': num_instances_available,
-                            'num_authors': num_authors})
-
+    return render(request, 'index.html',
+                  context={'num_books': num_books,
+                           'num_instances': num_instances,
+                           'num_instances_available': num_instances_available,
+                           'num_authors': num_authors})
