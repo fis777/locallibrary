e957837a73fcb4d066fcbd5193ff6dc89475e21f
fis777 <fqibymeksm@protonmail.com>
2018-10-15 11:05:02 +0500

    add visit counter

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -10,11 +10,16 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a')
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     return render(request, 'index.html',
                   context={'num_books': num_books,
                            'num_instances': num_instances,
                            'num_instances_available': num_instances_available,
-                           'num_authors': num_authors})
+                           'num_authors': num_authors,
+                           'num_visits': num_visits})
 
 class BookListView(ListView):
     '''Отображение списка книг'''
