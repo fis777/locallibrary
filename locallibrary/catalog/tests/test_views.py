11ccb1edac2b264c7227b98620dd5cfba5d9772b
fis777 <fis777@gmail.com>
2018-10-20 22:12:50 +0500

    test autorization

--- locallibrary/catalog/tests/test_views.py
+++ locallibrary/catalog/tests/test_views.py
@@ -1,6 +1,9 @@
+import datetime
 from django.test import TestCase
 from django.urls import reverse
-from catalog.models import Author
+from django.utils import timezone
+from django.contrib.auth.models import User #Required to assign User as a borrower
+from catalog.models import BookInstance, Book, Genre, Language, Author
 
 class AuthorListViewTest(TestCase):
     '''Тест отображения списка авторов '''
@@ -19,12 +22,12 @@ class AuthorListViewTest(TestCase):
     def test_view_url_accessible_by_name(self):
         resp = self.client.get(reverse('authors'))
         self.assertEqual(resp.status_code, 200)
-    
+
     def test_view_uses_correct_template(self):
         resp = self.client.get(reverse('authors'))
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, template_name='catalog/author_list.html')
-    
+
     def test_pagination_is_ten(self):
         resp = self.client.get(reverse('authors'))
         self.assertEqual(resp.status_code, 200)
@@ -40,3 +43,49 @@ class AuthorListViewTest(TestCase):
         self.assertTrue(resp.context['is_paginated'] is True)
         self.assertTrue(len(resp.context['author_list']) == 3)
 
+class LoanedBookInstancesByUserListViewTest(TestCase):
+
+    def setUp(self):
+        #используем setUp когда объект будет меняться
+        #Создание двух пользователей
+        test_user1 = User.objects.create_user(username='testuser1', password='12345')
+        test_user1.save()
+        test_user2 = User.objects.create_user(username='testuser2', password='12345')
+        test_user2.save()
+
+        #Создание книги
+        test_author = Author.objects.create(first_name='John', last_name='Smith')
+        test_genre = Genre.objects.create(name='Fantasy')
+        test_language = Language.objects.create(name='English')
+        test_book = Book.objects.create(title='Book Title', summary='My book summary', isbn='ABCDEFG', author=test_author, language=test_language)
+        # Create genre as a post-step
+        genre_objects_for_book = Genre.objects.all()
+        test_book.genre = genre_objects_for_book
+        test_book.save()
+
+        #Создание 30 объектов BookInstance
+        number_of_book_copies = 30
+        for book_copy in range(number_of_book_copies):
+            return_date = timezone.now() + datetime.timedelta(days=book_copy%5)
+            if book_copy % 2:
+                the_borrower = test_user1
+            else:
+                the_borrower = test_user2
+            status = 'm'
+            BookInstance.objects.create(book=test_book, imprint='Unlikely Imprint, 2016', due_back=return_date, borrower=the_borrower, status=status)
+
+    def test_redirect_if_not_logged_in(self):
+        resp = self.client.get(reverse('my-borrowed'))
+        self.assertRedirects(resp, '/accounts/login/?next=/catalog/mybooks/')
+
+    def test_logged_in_uses_correct_template(self):
+        self.client.login(username='testuser1', password='12345')
+        resp = self.client.get(reverse('my-borrowed'))
+
+        #Проверка что пользователь залогинился
+        self.assertEqual(str(resp.context['user']), 'testuser1')
+        #Check that we got a response "success"
+        self.assertEqual(resp.status_code, 200)
+
+        #Проверка того, что мы используем правильный шаблон
+        self.assertTemplateUsed(resp, 'catalog/bookinstance_list_borrowed_user.html')
